c943fc9c5523ea08fa5fa014be0644dc959da1b6
João Vitor <joaovitor>
2025-02-20 17:54:45 -0300

    QuasePronto

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Calculado-Web
+# Calculadora Web
 Um pequeno projeto para consolidar alguns conceitos básicos de HTML, CSS, JS e lógica de programação.
--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 function addAoDisplay(value) {
     document.getElementById("display").value += value
 }
+
+function addCalculoDisplay() {
+   calculo =  eval(document.getElementById("display").value)
+   document.getElementById("display").value  = calculo
+}
+
+function limpaDisplay() {
+    document.getElementById("display").value = ""
+}
